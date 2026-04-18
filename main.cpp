f5cb0772133fb7e4a797b4063df4c71304d11afe
Peter176-George <2400172@eng.asu.edu.eg>
2026-04-18 23:05:59 +0200

    Revert "Update main.cpp"
    This reverts commit 5b4524f017ca13d5595c46e15ab7d0c73af49e28.

--- main.cpp
+++ main.cpp
@@ -14,6 +14,10 @@ int main()
     cout<<"your are mohamed"<<endl;
     cout<<"I'm peter"<<endl;
     //how to write a code Note: this is a comment in main program
+<<<<<<< HEAD
     cout<<function(10)<<endl;
+=======
+    cout<<"Mohamed nabil ali"<<endl;
+>>>>>>> ab7797131196d3b137015e1ac39e833a0138b8e1
     return 0;
 }
