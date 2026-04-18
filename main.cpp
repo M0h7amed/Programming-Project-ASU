f2adff1115048ee1e19bbb0537c48147bf3095e6
Peter176-George <2400172@eng.asu.edu.eg>
2026-04-18 23:09:13 +0200

    edit 10

--- main.cpp
+++ main.cpp
@@ -3,5 +3,11 @@ using namespace std;
 int main()
 {
     cout<<"Peter "<<endl;
+    cout<<"your are mohamed"<<endl;
+    cout<<"I'm peter"<<endl;
+    //how to write a code Note: this is a comment in main program
+    cout<<function(10)<<endl;
+    cout<<"Mohamed ali"<<endl;
+    
     return 0;
 }
