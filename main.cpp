311f0ce13346f5b75cf64ac4548ce98d28c0361a
Mohamed Nabil <110033223+M0h7amed@users.noreply.github.com>
2026-04-24 20:07:31 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,11 @@
 #include <iostream>
 #include <math.h>
-
 using namespace std;
-int function(double x)
-{
-    return pow(x,2);
-}
+
 int main()
 {
-    cout<<"Mohamed"<<endl;
-     cout<<5+3<<endl;
-     cout<<5+5<<endl;
-    cout<<"Peter "<<endl;
-    cout<<"your are mohamed"<<endl;
-    cout<<"I'm peter"<<endl;
-    //how to write a code Note: this is a comment in main program
+    cout<<"hello world"<<endl;
     
-    cout<<function(20)<<endl;
     
     return 0;
 }
