f2f7e319950c537fc63f451e7679895d865ceb9f
Mohamed Nabil <110033223+M0h7amed@users.noreply.github.com>
2026-04-25 11:58:57 +0300

    Merging to files part 1 and part 2
    by merging to files and edit the variable to be suitable and make two codes work together

---
+++ classes.h
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <string>
+#pragma once
+using namespace std;
+
+class Motor
+{
+public:
+    string name;
+    float torque_Nm;
+    float speed_rpm;
+    float mass_kg;
+    float diameter_mm;
+    float width_mm;
+
+    Motor(string n, float t, float s, float m, float d, float w)
+        : name(n), torque_Nm(t), speed_rpm(s),
+          mass_kg(m), diameter_mm(d), width_mm(w) {}
+
+    void show() const
+
+    {
+        cout << "  Motor      : " << name << "\n";
+        cout << "  Torque     : " << torque_Nm   << " N'm\n";
+        cout << "  Speed      : " << speed_rpm   << " RPM\n";
+        cout << "  Mass       : " << mass_kg     << " kg\n";
+        cout << "  Diameter   : " << diameter_mm << " mm\n";
+        cout << "  Width      : " << width_mm    << " mm\n";
+    }
+};
+
+
+
+
+
+class Material
+{
+public:
+string name;
+double yieldStrength; //MPa
+double density;  //g/cm^3
+
+void setData(string n, double y, double d) //used to add new material to the database
+   {
+    name = n;
+    yieldStrength = y;
+    density = d;
+   }
+};
+
+
+
+
+
+class Gearbox
+{
+public:
+    string name;
+    float ratio;
+    float efficiency;
+    float mass_kg;
+    float diameter_mm;
+    float width_mm;
+
+    Gearbox(string n, float r, float e, float m, float d, float w)
+        : name(n), ratio(r), efficiency(e),
+          mass_kg(m), diameter_mm(d), width_mm(w) {}
+
+    void show() const
+    {
+        cout << "  Gearbox    : " << name        << "\n";
+        cout << "  Ratio      : " << ratio       << "\n";
+        cout << "  Efficiency : " << efficiency * 100 << " %\n";
+        cout << "  Mass       : " << mass_kg     << " kg\n";
+        cout << "  Diameter   : " << diameter_mm << " mm\n";
+        cout << "  Width      : " << width_mm    << " mm\n";
+    }
+};
+
--- main.cpp
+++ main.cpp
@@ -1,31 +1,37 @@
 #include <iostream>
 #include <string>
 #include <cmath>
-#include<string>
 #include<fstream>
+#include "classes.h"
+#include <vector>
 
 using namespace std;
 
 //Constants
 
-double g = 9.81, pi = 3.14159;
+const double g = 9.81, pi = 3.14159;
 
 //Classes
 
-class Material
-{
-public:
-string name;
-double yieldStrength; //MPa
-double density;  //g/cm^3
 
-void setData(string n, double y, double d) //used to add new material to the database
-   {
-    name = n;
-    yieldStrength = y;
-    density = d;
-   }
+
+struct Combination
+{
+    const Motor * motor;
+    const Gearbox * gearbox;
+    float output_torque_Nm;
+    float output_speed_rpm;
+    float cost;
 };
+
+float computeCost(const Motor& m, const Gearbox& g)
+{
+    float total_mass     = m.mass_kg     + g.mass_kg;
+    float total_diameter = m.diameter_mm + g.diameter_mm;
+    float total_width    = m.width_mm    + g.width_mm;
+    return total_mass + total_diameter / 100.0 + total_width / 100.0;
+}
+
 
 //functions
 
@@ -270,94 +276,10 @@ break;
                   }
                   delete[] database;
 
-return 0 ;
-}
-
-  
-#include <iostream>
-#include <vector>
-#include <cmath>
-#include <string>
-
-using namespace std;
 
 
-class Motor
-{
-public:
-    string name;
-    float torque_Nm;
-    float speed_rpm;
-    float mass_kg;
-    float diameter_mm;
-    float width_mm;
 
-    Motor(string n, float t, float s, float m, float d, float w)
-        : name(n), torque_Nm(t), speed_rpm(s),
-          mass_kg(m), diameter_mm(d), width_mm(w) {}
-
-    void show() const
-
-    {
-        cout << "  Motor      : " << name << "\n";
-        cout << "  Torque     : " << torque_Nm   << " N'm\n";
-        cout << "  Speed      : " << speed_rpm   << " RPM\n";
-        cout << "  Mass       : " << mass_kg     << " kg\n";
-        cout << "  Diameter   : " << diameter_mm << " mm\n";
-        cout << "  Width      : " << width_mm    << " mm\n";
-    }
-};
-
-class Gearbox
-{
-public:
-    string name;
-    float ratio;
-    float efficiency;
-    float mass_kg;
-    float diameter_mm;
-    float width_mm;
-
-    Gearbox(string n, float r, float e, float m, float d, float w)
-        : name(n), ratio(r), efficiency(e),
-          mass_kg(m), diameter_mm(d), width_mm(w) {}
-
-    void show() const
-    {
-        cout << "  Gearbox    : " << name        << "\n";
-        cout << "  Ratio      : " << ratio       << "\n";
-        cout << "  Efficiency : " << efficiency * 100 << " %\n";
-        cout << "  Mass       : " << mass_kg     << " kg\n";
-        cout << "  Diameter   : " << diameter_mm << " mm\n";
-        cout << "  Width      : " << width_mm    << " mm\n";
-    }
-};
-
-
-struct Combination
-{
-    const Motor * motor;
-    const Gearbox * gearbox;
-    float output_torque_Nm;
-    float output_speed_rpm;
-    float cost;
-};
-
-float computeCost(const Motor& m, const Gearbox& g)
-{
-    float total_mass     = m.mass_kg     + g.mass_kg;
-    float total_diameter = m.diameter_mm + g.diameter_mm;
-    float total_width    = m.width_mm    + g.width_mm;
-    return total_mass + total_diameter / 100.0 + total_width / 100.0;
-}
-
-const float g=9.81;
-
-int main()
-
-{
-
-    vector<Motor> motors = {
+vector<Motor> motors = {
         Motor("Maxon EC-i 40 (70W)",  0.180, 8500, 0.340, 40.0, 73.0),
         Motor("Maxon RE 35 (90W)",  0.253, 6500, 0.530, 35.0, 94.0)
     };
@@ -375,16 +297,8 @@ int main()
 
     float ml,L,mp,alpha_max,omega_required;
 
-    cout << "Enter link mass  ml (kg): ";
-    cin >> ml;
-    cout << "Enter link length  L (mm): ";
-    cin >> L;
-    cout << "Enter payload mass mp (kg): ";
-    cin >> mp;
-    cout << "Enter max angular acc.  alpha_max (rad/s*s): ";
-    cin >> alpha_max;
-    cout << "Enter required output speed Omega (RPM): ";
-    cin >> omega_required;
+
+    
 
 
     float T_required =(ml*g*(L/2))+(mp*g*L+ml*(L/2)*(L/2)*alpha_max)+(mp*L*L*alpha_max);
@@ -456,5 +370,21 @@ int main()
 
     }
 
-    return 0;
+
+
+return 0;
 }
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
